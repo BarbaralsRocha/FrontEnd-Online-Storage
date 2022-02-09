cb6cbeec51a7809fd8419074e148b2ef834d6805
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-02-09 15:32:42 -0300

    Requisito Finalizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 export async function getCategories() {
-  // Implemente aqui
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories').then((response) => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery( categoryId, query) {
+  const address = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
+  return fetch(address).then((response) => response.json());
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
