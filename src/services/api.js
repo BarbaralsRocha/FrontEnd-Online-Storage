d553122fe8728d3ea933c12e6c95c54b6744b1db
AlanFernandes <josefernandes.alan@gmail.com>
2022-02-09 16:23:37 -0300

    First commit req 04

--- src/services/api.js
+++ src/services/api.js
@@ -5,5 +5,5 @@ export async function getCategories() {
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const address = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
   return fetch(address).then((response) => response.json());
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe.
 }
