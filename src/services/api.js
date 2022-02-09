4e5c84c43a459b193e9de56f1b976d807751a951
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-02-09 15:36:40 -0300

    Erro no lint corrigido

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories').then((response) => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery( categoryId, query) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const address = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
   return fetch(address).then((response) => response.json());
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
