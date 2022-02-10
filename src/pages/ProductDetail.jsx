96596a58b51a88273f015b8fc6ca56e41862d1ef
Barbara-Rocha <barbaraluizasr@gmail.com>
2022-02-10 17:04:35 -0300

    Req 9

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getProduct } from '../services/api';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+
+import { getProduct,
+  addProductToCart,
+} from '../services/api';
 
 export default class ProductDetail extends React.Component {
   state = {
@@ -14,15 +19,27 @@ export default class ProductDetail extends React.Component {
     const product = await getProduct(id);
     this.setState({ product });
   }
+
   //  nome do produto, imagem, preço e especificação técnica
 
   render() {
-    const { product: { title, price, thumbnail, attributes } } = this.state;
+    const { product } = this.state;
+    const { title, price, thumbnail, attributes } = product;
     return (
       <div>
         <p data-testid="product-detail-name">{title}</p>
         <p>{price}</p>
         <img src={ thumbnail } alt={ title } />
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ () => addProductToCart(product) }
+        >
+          Adicionar ao carrinho
+        </button>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          <AiOutlineShoppingCart />
+        </Link>
         <table>
           {
             attributes.map(({ name, value_name: valueName, id }) => (
