cec023238f960fa2d8657b2cd52e130e8604f0b6
AlanFernandes <josefernandes.alan@gmail.com>
2022-02-09 22:36:30 -0300

    corrige req 03

--- src/pages/ShopCart.jsx
+++ src/pages/ShopCart.jsx
@@ -17,17 +17,21 @@ export default class ShopCart extends React.Component {
     return (
       <div>
         {
-          products.map(({ title, thumbnail, price, id }) => (
-            <div key={ id }>
-              <p data-testid="shopping-cart-product-name">{title}</p>
-              <img src={ thumbnail } alt={ title } />
-              <p>
-                $
-                {price}
-              </p>
-              <p data-testid="shopping-cart-product-quantity">1</p>
-            </div>
-          ))
+          !products.length
+            ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+            : (
+              products.map(({ title, thumbnail, price, id }) => (
+                <div key={ id }>
+                  <p data-testid="shopping-cart-product-name">{title}</p>
+                  <img src={ thumbnail } alt={ title } />
+                  <p>
+                    $
+                    {price}
+                  </p>
+                  <p data-testid="shopping-cart-product-quantity">1</p>
+                </div>
+              ))
+            )
         }
       </div>
     );
